78c776492e47a2f541771c61d727561f596b4671
Lucas Morais <lucascdemorais@gmail.com>
2023-04-02 12:31:13 -0300

    init

---
+++ src/main/java/com/cda/multitenant/TenantAwareTaskDecorator.java
@@ -0,0 +1,20 @@
+package com.cda.multitenant;
+
+import org.springframework.core.task.TaskDecorator;
+import org.springframework.lang.NonNull;
+
+public class TenantAwareTaskDecorator implements TaskDecorator {
+    @Override
+    @NonNull
+    public Runnable decorate(@NonNull Runnable runnable) {
+        String tenantId = TenantContext.getTenantId();
+        return () -> {
+            try {
+                TenantContext.setTenantId(tenantId);
+                runnable.run();
+            } finally {
+                TenantContext.setTenantId(null);
+            }
+        };
+    }
+}
